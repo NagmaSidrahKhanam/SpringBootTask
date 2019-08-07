ad6e5d5be1a57ceb493388eb18a758317644f8c1
khsidra <nagma.sidra@gmail.com>
2019-08-07 09:44:22 +0530

    updated pom.xml

--- src/main/java/com/stackroute/muzix/model/Track.java
+++ src/main/java/com/stackroute/muzix/model/Track.java
@@ -1,9 +1,5 @@
 package com.stackroute.muzix.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
